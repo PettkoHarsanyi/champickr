85e6438a1ece51151f8969fa9067936b5220cdce
PettkoHarsanyi <pettko.harsanyi@gmail.com>
2023-05-09 04:43:30 +0200

    Champ selector almost done, filtering done

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,69 @@ const inter = Inter({
 
 export default function Home() {
 
+  const PICLINK = "http://ddragon.leagueoflegends.com/cdn/13.9.1/img/champion/";
   const [champions, setChampions] = useState(championsJson);
   const [index, setIndex] = useState(0);
   const [firstPicked, setFirstPicked] = useState(false);
   const [lastPicked, setLastPicked] = useState(null);
+
+  const [champSelect, setChampSelect] = useState(Object.values(championsJson));
+
+  const [teamBlue, setTeamBlue] = useState({
+    "player1": {
+      "champion": {},
+      "role": {},
+      "isPicked": false,
+    },
+    "player2": {
+      "champion": {},
+      "role": {},
+      "isPicked": false,
+    },
+    "player3": {
+      "champion": {},
+      "role": {},
+      "isPicked": false,
+    },
+    "player4": {
+      "champion": {},
+      "role": {},
+      "isPicked": false,
+    },
+    "player5": {
+      "champion": {},
+      "role": {},
+      "isPicked": false,
+    }
+  })
+
+  const [teamRed, setTeamRed] = useState({
+    "player1": {
+      "champion": {},
+      "role": {},
+      "isPicked": false,
+    },
+    "player2": {
+      "champion": {},
+      "role": {},
+      "isPicked": false,
+    },
+    "player3": {
+      "champion": {},
+      "role": {},
+      "isPicked": false,
+    },
+    "player4": {
+      "champion": {},
+      "role": {},
+      "isPicked": false,
+    },
+    "player5": {
+      "champion": {},
+      "role": {},
+      "isPicked": false,
+    }
+  })
 
   const [loaderProps, setLoaderProps] = useState({
     opacity: 1,
@@ -69,16 +128,21 @@ export default function Home() {
       champRows[i].classList.add("champRowsFull");
     }
 
-    const selectorDiv = document.getElementById("selector");
-    selectorDiv.classList.add("selectorFull");
+
+    const input = document.getElementById("inputname");
+    input.style.visibility = "visible";
+    input.focus();
   }
 
   const pick = (event) => {
     event.stopPropagation();
 
-    if(lastPicked==null){
-    }else{
-      if(!event.target.isSameNode(lastPicked)){
+    const selectorDiv = document.getElementById("selector");
+    selectorDiv.classList.add("selectorFull");
+
+    if (lastPicked == null) {
+    } else {
+      if (!event.target.isSameNode(lastPicked)) {
         lastPicked.closest(".champrow").classList.remove("champrowselected");
       }
     }
@@ -87,11 +151,35 @@ export default function Home() {
       pickFirst();
     }
 
+
+    const input = document.getElementById("inputname");
+    input.value = "";
+    input.focus();
+    setChampSelect(Object.values(championsJson));
+    input.addEventListener("focusout", function () {
+      let _this = this;
+      setTimeout(function () {
+        _this.focus();
+      }, 0);
+    })
+
     event.target.closest(".champrow").classList.add("champrowselected");
 
     // console.log(lastPicked);
     setLastPicked(event.target);
-    document.getElementById("inputname").focus();
+  }
+
+  const handleInput = (event) => {
+    setChampSelect(Object.values(champions).filter((champ) =>
+      champ.name.toUpperCase().includes(event.target.value.toUpperCase())
+    ))
+  }
+
+  const handleSelect = (champion) => {
+    const selectorDiv = document.getElementById("selector");
+    selectorDiv.classList.remove("selectorFull")
+    const input = document.getElementById("inputname");
+    input.value = "";
   }
 
   return (
@@ -103,43 +191,22 @@ export default function Home() {
           <div style={{ fontSize: "8vh" }}>CHAMPICKR</div>
           <div>Calculate the class and subclass of the champion you have the best winning chance with, based on the enemy dominating class.</div>
         </div>
+
+
         <div id="championselect" className={`flex flex-row w-full championselect`}>
+
           <div className={`w-[30%] flex flex-col champRows`}>
-            <div className='h-1/5 grborder1 flex flex-row p-3 gap-3 items-center champrow champrowleft'>
-              <div className='h-full aspect-square relative' style={{ border: "0.2vh solid #b99c6a" }}>
-                <Image className='champpic' src={champions[index].icon} fill sizes='50px' style={{ filter: "grayscale(100%)" }} alt='champ' />
-                <div className='picshadow' onClick={(event) => pick(event)}></div>
-              </div>
-              <div className='h-3/4 m-5 aspect-square rounded-full' style={{ border: "0.2vh solid #b99c6a" }}></div>
-            </div>
-            <div className='h-1/5 grborder2 flex flex-row p-3 gap-3 items-center champrow champrowleft'>
-              <div className='h-full aspect-square relative' style={{ border: "0.2vh solid #b99c6a" }}>
-                <Image className='champpic' src={champions[(index + 1) % 152].icon} fill sizes='50px' style={{ filter: "grayscale(100%)" }} alt='champ' />
-                <div className='picshadow' onClick={(event) => pick(event)}></div>
-              </div>
-              <div className='h-3/4 m-5 aspect-square rounded-full' style={{ border: "0.2vh solid #b99c6a" }}></div>
-            </div>
-            <div className='h-1/5 grborder3 flex flex-row p-3 gap-3 items-center champrow champrowleft'>
-              <div className='h-full aspect-square relative' style={{ border: "0.2vh solid #b99c6a" }}>
-                <Image className='champpic' src={champions[(index + 2) % 152].icon} fill sizes='50px' style={{ filter: "grayscale(100%)" }} alt='champ' />
-                <div className='picshadow' onClick={(event) => pick(event)}></div>
-              </div>
-              <div className='h-3/4 m-5 aspect-square rounded-full' style={{ border: "0.2vh solid #b99c6a" }}></div>
-            </div>
-            <div className='h-1/5 grborder4 flex flex-row p-3 gap-3 items-center champrow champrowleft'>
-              <div className='h-full aspect-square relative' style={{ border: "0.2vh solid #b99c6a" }}>
-                <Image className='champpic' src={champions[(index + 3) % 152].icon} fill sizes='50px' style={{ filter: "grayscale(100%)" }} alt='champ' />
-                <div className='picshadow' onClick={(event) => pick(event)}></div>
-              </div>
-              <div className='h-3/4 m-5 aspect-square rounded-full' style={{ border: "0.2vh solid #b99c6a" }}></div>
-            </div>
-            <div className='h-1/5 grborder5 flex flex-row p-3 gap-3 items-center champrow champrowleft'>
-              <div className='h-full aspect-square relative' style={{ border: "0.2vh solid #b99c6a" }}>
-                <Image className='champpic' src={champions[(index + 4) % 152].icon} fill sizes='50px' style={{ filter: "grayscale(100%)" }} alt='champ' />
-                <div className='picshadow' onClick={(event) => pick(event)}></div>
-              </div>
-              <div className='h-3/4 m-5 aspect-square rounded-full' style={{ border: "0.2vh solid #b99c6a" }}></div>
-            </div>
+            {Object.values(teamBlue).map((player, playerIndex) => {
+              return (
+                <div className={`h-1/5 grborder${playerIndex + 1} flex flex-row p-3 gap-3 items-center champrow champrowleft`}>
+                  <div className='h-full aspect-square relative' style={{ border: "0.2vh solid #b99c6a" }}>
+                    <Image className='champpic' src={player.isPicked ? (PICLINK + player.champion.image.full) : (PICLINK + Object.values(champions)[(index + playerIndex) % Object.keys(champions).length].image.full)} fill sizes='50px' style={{ filter: "grayscale(100%)" }} alt='champ' />
+                    <div className='picshadow' onClick={(event) => pick(event)}></div>
+                  </div>
+                  <div className='h-3/4 m-5 aspect-square rounded-full' style={{ border: "0.2vh solid #b99c6a" }}></div>
+                </div>
+              )
+            })}
           </div>
 
           <div className='w-[40%] flex flex-col align-middle justify-center relative'>
@@ -151,17 +218,17 @@ export default function Home() {
             </div>
             <div id='selector' className='absolute h-4/5 border border-red-800 left-0 right-0 m-auto selector'>
               <div className='searchbar flex flex-row justify-between align-middle'>
-                <input id="inputname" type='text' placeholder='Start typing...' className='w-4/5 inputfield' style={{ flex: 1 }} />
+                <input onChange={handleInput} id="inputname" type='text' placeholder='Start typing...' className='w-4/5 inputfield' style={{ flex: 1 }} autoFocus />
                 <div style={{ width: "50px", margin: "2vh" }}>
                   <Image src={magn} alt='magnifyer' />
                 </div>
               </div>
               <div className='champlist container m-auto '>
                 <div className='w-full grid grid-cols-7 gap-1 champsscroll'>
-                  {champions.map((champion, index) => {
+                  {Object.values(champSelect).map((champion, index) => {
                     return (
                       <div className='champ' key={champion.key}>
-                        <Image src={champion.icon} width={100} height={100} alt={champion.name} style={{ objectFit: "contain" }} />
+                        <Image onClick={() => handleSelect(champion)} src={PICLINK + champion.image.full} width={100} height={100} alt={champion.name} style={{ objectFit: "contain" }} />
                       </div>
                     )
                   })}
@@ -170,44 +237,22 @@ export default function Home() {
             </div>
           </div>
 
-          <div id="champRows" className='w-[30%] flex flex-col gap-3 champRows'>
-            <div className='h-1/5 grborder6 flex flex-row-reverse p-3 gap-3 items-center champrow champrowright'>
-              <div className='h-full aspect-square relative' style={{ border: "0.2vh solid #b99c6a" }}>
-                <Image className='champpic' src={champions[(index + 5) % 152].icon} fill sizes='50px' style={{ filter: "grayscale(100%)" }} alt='champ' />
-                <div className='picshadow' onClick={(event) => pick(event)}></div>
-              </div>
-              <div className='h-3/4 m-5 aspect-square rounded-full' style={{ border: "0.2vh solid #b99c6a" }}></div>
-            </div>
-            <div className='h-1/5 grborder7 flex flex-row-reverse p-3 gap-3 items-center champrow champrowright'>
-              <div className='h-full aspect-square relative' style={{ border: "0.2vh solid #b99c6a" }}>
-                <Image className='champpic' src={champions[(index + 6) % 152].icon} fill sizes='50px' style={{ filter: "grayscale(100%)" }} alt='champ' />
-                <div className='picshadow' onClick={(event) => pick(event)}></div>
-              </div>
-              <div className='h-3/4 m-5 aspect-square rounded-full' style={{ border: "0.2vh solid #b99c6a" }}></div>
-            </div>
-            <div className='h-1/5 grborder8 flex flex-row-reverse p-3 gap-3 items-center champrow champrowright'>
-              <div className='h-full aspect-square relative' style={{ border: "0.2vh solid #b99c6a" }}>
-                <Image className='champpic' src={champions[(index + 7) % 152].icon} fill sizes='50px' style={{ filter: "grayscale(100%)" }} alt='champ' />
-                <div className='picshadow' onClick={(event) => pick(event)}></div>
-              </div>
-              <div className='h-3/4 m-5 aspect-square rounded-full' style={{ border: "0.2vh solid #b99c6a" }}></div>
-            </div>
-            <div className='h-1/5 grborder9 flex flex-row-reverse p-3 gap-3 items-center champrow champrowright'>
-              <div className='h-full aspect-square relative' style={{ border: "0.2vh solid #b99c6a" }}>
-                <Image className='champpic' src={champions[(index + 8) % 152].icon} fill sizes='50px' style={{ filter: "grayscale(100%)" }} alt='champ' />
-                <div className='picshadow' onClick={(event) => pick(event)}></div>
-              </div>
-              <div className='h-3/4 m-5 aspect-square rounded-full' style={{ border: "0.2vh solid #b99c6a" }}></div>
-            </div>
-            <div className='h-1/5 grborder10 flex flex-row-reverse p-3 gap-3 items-center champrow champrowright'>
-              <div className='h-full aspect-square relative' style={{ border: "0.2vh solid #b99c6a" }}>
-                <Image className='champpic' src={champions[(index + 9) % 152].icon} fill sizes='50px' style={{ filter: "grayscale(100%)" }} alt='champ' />
-                <div className='picshadow' onClick={(event) => pick(event)}></div>
-              </div>
-              <div className='h-3/4 m-5 aspect-square rounded-full' style={{ border: "0.2vh solid #b99c6a" }}></div>
-            </div>
+          <div className='w-[30%] flex flex-col gap-3 champRows'>
+            {Object.values(teamRed).map((player, playerIndex) => {
+              return (
+                <div className={`h-1/5 grborder${playerIndex + 6} flex flex-row-reverse p-3 gap-3 items-center champrow champrowright`}>
+                  <div className='h-full aspect-square relative' style={{ border: "0.2vh solid #b99c6a" }}>
+                    <Image className='champpic' src={player.isPicked ? (PICLINK + player.champion.image.full) : (PICLINK + Object.values(champions)[(index + playerIndex) % Object.keys(champions).length].image.full)} fill sizes='50px' style={{ filter: "grayscale(100%)" }} alt='champ' />
+                    <div className='picshadow' onClick={(event) => pick(event)}></div>
+                  </div>
+                  <div className='h-3/4 m-5 aspect-square rounded-full' style={{ border: "0.2vh solid #b99c6a" }}></div>
+                </div>
+              )
+            })}
           </div>
         </div>
+
+
       </div>
 
       <div id='loader' style={loaderProps} className="absolute top-0 left-0 z-10 w-full h-full items-center flex flex-col font-beaufort align-middle justify-center text-center loader">
